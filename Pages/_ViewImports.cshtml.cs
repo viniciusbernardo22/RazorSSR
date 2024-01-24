6896e42234303d00a513074654c77577f60bff52
Vinicius <vini383@gmail.com>
2024-01-24 14:38:41 -0300

    feat: added blazor bootstrap deps

---
+++ Pages/_ViewImports.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor.Pages
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet() 
+        {
+        }
+        
+    }
+}
